3c6c0c5aaf857c8368b8e7d567bba624d3ce6bce
mrbjg <harshsharmag999@gmail.com>
2026-07-11 12:18:17 +0530

    security: move Firebase credentials to .env, add .env to gitignore

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4YIIGBQL2uT3CoHbTdmd0N8fJAz_ZhYA",
-  authDomain: "cartelcypher-demo.firebaseapp.com",
-  projectId: "cartelcypher-demo",
-  storageBucket: "cartelcypher-demo.firebasestorage.app",
-  messagingSenderId: "573784135804",
-  appId: "1:573784135804:web:84c25dd6c79ddbff8fd1f9",
-  measurementId: "G-RSCMBTZXGB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
